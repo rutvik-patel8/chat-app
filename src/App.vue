c4c91271b17a59e8a0ca3009090eade139c8eb44
Mayank Singh <mayank.singh@bacancy.com>
2022-11-18 13:40:25 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,100 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <h2>Chat App</h2>
+    <input
+      v-if="isInput"
+      type="text"
+      placeholder="enter message"
+      v-model="name"
+      @keyup.enter="setName()"
+    />
+    <div v-else>
+      NAME: <b>{{ name }}</b>
     </div>
-    <router-view/>
+    <br />
+    <h3>{{ description }}</h3> <br>
+    <div v-for="msg in messages" :key="msg.id">
+      <div>
+        <b>{{ msg.name }}</b>:{{ msg.msg }}
+      </div><br>
+    </div>
+    <input
+      type="text"
+      placeholder="enter message"
+      v-model="input"
+      @keyup.enter="sendMsg()"
+    />
   </div>
 </template>
+
+<script>
+import { io } from "socket.io-client";
+const URL = "http://localhost:3000";
+const socket = io(URL);
+export default {
+  data() {
+    return {
+      list: [],
+      totalUserConnected: 0,
+      description: "",
+      messages: [],
+      input: "",
+      name: "",
+      isInput: true,
+    };
+  },
+  created() {
+    fetch("http://localhost:3000/history")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.messages = res;
+      });
+  },
+  mounted() {
+    this.name = localStorage.getItem("name") || "";
+    if (this.name) {
+      this.isInput = false;
+    }
+    socket.on("connect", () => {
+      socket.username = "Rutvik";
+      console.log(socket);
+    });
+
+    socket.on("total", (data) => {
+      console.log(data);
+      this.totalUserConnected = data.total;
+    });
+
+    socket.on("newclientconnect", (data) => {
+      this.description = data.description;
+    });
+
+    socket.on("new-message", (data) => {
+      console.log(data);
+      this.messages.push(data);
+    });
+  },
+  methods: {
+    sendMsg() {
+      this.messages.push({
+        name: this.name,
+        msg: this.input,
+      });
+      socket.emit("new-message", {
+        name: this.name,
+        msg: this.input,
+      });
+      this.input = "";
+    },
+    setName() {
+      localStorage.setItem("name", this.name);
+      this.isInput = false;
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -15,18 +103,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: rgb(226, 226, 226);
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
+import Vue from "vue";
+import App from "./App.vue";
 Vue.config.productionTip = false
+// import wrap from "@vue/web-component-wrapper";
+
+// const wrappedElement = wrap(Vue, App);
+
+// window.customElements.define("chat-app", wrappedElement);
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
+
+
+
